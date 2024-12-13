fae7e14394532ddbc358ff3c91f8a157698971c6
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-12-12 23:43:58 -0300

    Ajustes das actions - desabilitadas por erro relacionado ao supabsae

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    // {
+    //   path: 'header',
+    //   loadChildren: () => import('./header/header.module').then(module => module.HeaderModule)
+    // }, {
+    //   path: 'footer',
+    //   loadChildren: () => import('./footer/footer.module').then(module => module.FooterModule)
+    // }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
